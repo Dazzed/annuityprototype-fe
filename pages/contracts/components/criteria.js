5a118833c8673e878bdd2072ebe7622a692db3c0
Dazzed <sameepsheth@gmail.com>
2020-10-01 18:12:10 +0000

    ui update

--- pages/contracts/components/criteria.js
+++ pages/contracts/components/criteria.js
@@ -9,30 +9,42 @@ export default function Criteria() {
       </li>
       <li>
         <a href="#">Contract List View</a>
+        <input type="text" placeholder=""className="form-field"  />
       </li>
       <li>
         <a href="#">Contract#/Name</a>
+        <input type="text" placeholder=""className="form-field"  />
       </li>
       <li>
         <a href="#">Client Age</a>
+        <label>Min</label>
+        <input type="text" placeholder=""className="form-field"  />
+        <label>Max</label>
+        <input type="text" placeholder=""className="form-field"  />
       </li>
       <li>
         <a href="#">Investment Option</a>
+        <input type="text" placeholder=""className="form-field"  />
       </li>
       <li>
         <a href="#">Rider Settings</a>
+        <input type="text" placeholder=""className="form-field"  />
       </li>
       <li>
         <a href="#">Product Settings</a>
+        <input type="text" placeholder=""className="form-field"  />
       </li>
       <li>
         <a href="#">Program Settings</a>
+        <input type="text" placeholder=""className="form-field"  />
       </li>
       <li>
         <a href="#">Anniversary Date</a>
+        <input type="text" placeholder=""className="form-field"  />
       </li>
       <li>
         <a href="#">Contract Value</a>
+        <input type="text" placeholder=""className="form-field"  />
       </li>
       <div className="apply-section">
         <button>Apply</button>
