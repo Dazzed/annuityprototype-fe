9ee8f09aac43535fb89f9f75eb2314303334f366
Dazzed <sameepsheth@gmail.com>
2020-10-02 12:46:18 +0000

    basic filters

--- pages/contracts/components/criteria.js
+++ pages/contracts/components/criteria.js
@@ -1,31 +1,50 @@
 import { useState } from "react";
 
 export default function Criteria(props) {
-  let [contractNumberFilterOp, setContractNumberFilterOp] = useState("eq");
-  let [contractNumberFilterVal, setContractNumberFilterval] = useState("");
-  let [ownerFilterOp, setOwnerFilterOp] = useState("like");
+  let [contractNumberFilterOp, setContractNumberFilterOp] = useState("");
+  let [contractNumberFilterVal, setContractNumberFilterVal] = useState("");
+
+  let [ownerFilterOp, setOwnerFilterOp] = useState("");
   let [ownerFilterVal, setOwnerFilterVal] = useState("");
-  let [productNameFilterOp, setProductNameFilterOp] = useState("like");
+
+  let [annuitantFilterOp, setAnnuitantFilterOp] = useState("");
+  let [annuitantFilterVal, setAnnuitantFilterVal] = useState("");
+
+  let [productNameFilterOp, setProductNameFilterOp] = useState("");
   let [productNameFilterVal, setProductNameFilterVal] = useState("");
 
   const applyFilter = () => {
     const filters = {};
 
-    if (contractNumberFilterVal) {
+    if (contractNumberFilterVal && contractNumberFilterOp) {
       filters.contract_number = JSON.stringify([
         contractNumberFilterOp,
         contractNumberFilterVal,
       ]);
     }
 
-    if (ownerFilterVal) {
-      filters.owner = JSON.stringify([ownerFilterOp, `%${ownerFilterVal}%`]);
+    if (ownerFilterVal && ownerFilterOp) {
+      filters.owner = JSON.stringify([
+        ownerFilterOp,
+        ownerFilterOp === "like" ? `%${ownerFilterVal}%` : ownerFilterVal,
+      ]);
     }
 
-    if (productNameFilterVal) {
+    if (annuitantFilterVal && annuitantFilterOp) {
+      filters.annuitant = JSON.stringify([
+        annuitantFilterOp,
+        ownerFilterOp === "like"
+          ? `%${annuitantFilterVal}%`
+          : annuitantFilterVal,
+      ]);
+    }
+
+    if (productNameFilterVal && productNameFilterOp) {
       filters.product_name = JSON.stringify([
         productNameFilterOp,
-        `%${productNameFilterVal}%`,
+        productNameFilterOp === "like"
+          ? `%${productNameFilterVal}%`
+          : productNameFilterVal,
       ]);
     }
 
@@ -33,9 +52,17 @@ export default function Criteria(props) {
   };
 
   const resetFilter = () => {
-    setContractNumberFilterval("");
+    setContractNumberFilterVal("");
+    setContractNumberFilterOp("");
+
     setOwnerFilterVal("");
+    setOwnerFilterOp("");
+
     setProductNameFilterVal("");
+    setProductNameFilterOp("");
+
+    setAnnuitantFilterVal("");
+    setAnnuitantFilterOp("");
   };
 
   return (
@@ -47,44 +74,70 @@ export default function Criteria(props) {
         </a>
       </li>
       <div id="accordion">
-        <div class="card">
-          <div class="card-header cardcustomheader p-1" id="headingOne">
-            <h5 class="mb-0">
-              <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        <div className="card">
+          <div className="card-header cardcustomheader p-1" id="headingOne">
+            <h5 className="mb-0">
+              <button
+                className="btn btn-link"
+                data-toggle="collapse"
+                data-target="#collapseOne"
+                aria-expanded="true"
+                aria-controls="collapseOne"
+              >
                 Contract Number
               </button>
             </h5>
           </div>
-
-          <div id="collapseOne" class="collapse position-relative" aria-labelledby="headingOne" data-parent="#accordion">
-            <div class="card-body px-2 py-1">
+          <div
+            id="collapseOne"
+            className="collapse position-relative"
+            aria-labelledby="headingOne"
+            data-parent="#accordion"
+          >
+            <div className="card-body px-2 py-1">
               <input
                 type="text"
                 name="contractNumberFilterVal"
                 placeholder=""
                 className="form-field"
                 value={contractNumberFilterVal}
-                onChange={(e) => setContractNumberFilterval(e.target.value)}
+                onChange={(e) => setContractNumberFilterVal(e.target.value)}
               />
             </div>
-            <select className="dropdown-toggle-menu">
-              <option>section 1</option>
-              <option>section 2</option>
-              <option>section 3</option>
+            <select
+              className="dropdown-toggle-menu"
+              value={contractNumberFilterOp}
+              onChange={(e) => setContractNumberFilterOp(e.target.value)}
+            >
+              <option value=""></option>
+              <option value="eq">Equals</option>
+              <option value="ne">Not Equals</option>
+              <option value="like">Contains</option>
             </select>
           </div>
         </div>
-        <div class="card">
-          <div class="card-header cardcustomheader p-1" id="headingTwo">
-            <h5 class="mb-0">
-              <button class="btn btn-link" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseOne">
+
+        <div className="card">
+          <div className="card-header cardcustomheader p-1" id="headingTwo">
+            <h5 className="mb-0">
+              <button
+                className="btn btn-link"
+                data-toggle="collapse"
+                data-target="#collapseTwo"
+                aria-expanded="true"
+                aria-controls="collapseOne"
+              >
                 Owner
               </button>
             </h5>
           </div>
-
-          <div id="collapseTwo" class="collapse position-relative" aria-labelledby="headingTwo" data-parent="#accordion">
-            <div class="card-body px-2 py-1">
+          <div
+            id="collapseTwo"
+            className="collapse position-relative"
+            aria-labelledby="headingTwo"
+            data-parent="#accordion"
+          >
+            <div className="card-body px-2 py-1">
               <input
                 type="text"
                 name="ownerFilterVal"
@@ -94,24 +147,83 @@ export default function Criteria(props) {
                 onChange={(e) => setOwnerFilterVal(e.target.value)}
               />
             </div>
-            <select className="dropdown-toggle-menu">
-              <option>section 1</option>
-              <option>section 2</option>
-              <option>section 3</option>
+            <select
+              className="dropdown-toggle-menu"
+              value={ownerFilterOp}
+              onChange={(e) => setOwnerFilterOp(e.target.value)}
+            >
+              <option value=""></option>
+              <option value="eq">Equals</option>
+              <option value="ne">Not Equals</option>
+              <option value="like">Contains</option>
             </select>
           </div>
         </div>
-        <div class="card">
-          <div class="card-header cardcustomheader p-1" id="headingThree">
-            <h5 class="mb-0">
-              <button class="btn btn-link" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseOne">
+
+        <div className="card">
+          <div className="card-header cardcustomheader p-1" id="headingThree">
+            <h5 className="mb-0">
+              <button
+                className="btn btn-link"
+                data-toggle="collapse"
+                data-target="#annuitant_collapse"
+                aria-expanded="true"
+                aria-controls="annuitant_collapse"
+              >
+                Annuitant
+              </button>
+            </h5>
+          </div>
+          <div
+            id="annuitant_collapse"
+            className="collapse position-relative"
+            aria-labelledby="headingTne"
+            data-parent="#accordion"
+          >
+            <div className="card-body px-2 py-1">
+              <input
+                type="text"
+                name="annuitantFilterVal"
+                placeholder=""
+                className="form-field"
+                value={annuitantFilterVal}
+                onChange={(e) => setAnnuitantFilterVal(e.target.value)}
+              />
+            </div>
+            <select
+              className="dropdown-toggle-menu"
+              value={annuitantFilterOp}
+              onChange={(e) => setAnnuitantFilterOp(e.target.value)}
+            >
+              <option value=""></option>
+              <option value="eq">Equals</option>
+              <option value="ne">Not Equals</option>
+              <option value="like">Contains</option>
+            </select>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="card-header cardcustomheader p-1" id="headingThree">
+            <h5 className="mb-0">
+              <button
+                className="btn btn-link"
+                data-toggle="collapse"
+                data-target="#collapseThree"
+                aria-expanded="true"
+                aria-controls="collapseOne"
+              >
                 Product
               </button>
             </h5>
           </div>
-
-          <div id="collapseThree" class="collapse position-relative" aria-labelledby="headingTne" data-parent="#accordion">
-            <div class="card-body px-2 py-1">
+          <div
+            id="collapseThree"
+            className="collapse position-relative"
+            aria-labelledby="headingTne"
+            data-parent="#accordion"
+          >
+            <div className="card-body px-2 py-1">
               <input
                 type="text"
                 name="productNameFilterVal"
@@ -121,25 +233,41 @@ export default function Criteria(props) {
                 onChange={(e) => setProductNameFilterVal(e.target.value)}
               />
             </div>
-            <select className="dropdown-toggle-menu">
-              <option>section 1</option>
-              <option>section 2</option>
-              <option>section 3</option>
+            <select
+              className="dropdown-toggle-menu"
+              value={productNameFilterOp}
+              onChange={(e) => setProductNameFilterOp(e.target.value)}
+            >
+              <option value=""></option>
+              <option value="eq">Equals</option>
+              <option value="ne">Not Equals</option>
+              <option value="like">Contains</option>
             </select>
           </div>
         </div>
-        <div class="card">
-          <div class="card-header cardcustomheader p-1" id="headingFour">
-            <h5 class="mb-0">
-              <button class="btn btn-link" data-toggle="collapse" data-target="#collapseFour" aria-expanded="true" aria-controls="collapseOne">
+
+        <div className="card">
+          <div className="card-header cardcustomheader p-1" id="headingFour">
+            <h5 className="mb-0">
+              <button
+                className="btn btn-link"
+                data-toggle="collapse"
+                data-target="#collapseFour"
+                aria-expanded="true"
+                aria-controls="collapseOne"
+              >
                 Client Age
               </button>
             </h5>
           </div>
-
-          <div id="collapseFour" class="collapse position-relative" aria-labelledby="headingFour" data-parent="#accordion">
-            <div class="card-body px-2 py-1">
-              <div class="row">
+          <div
+            id="collapseFour"
+            className="collapse position-relative"
+            aria-labelledby="headingFour"
+            data-parent="#accordion"
+          >
+            <div className="card-body px-2 py-1">
+              <div className="row">
                 <div className="col-lg-6">
                   <label>Min</label>
                   <input
@@ -147,8 +275,6 @@ export default function Criteria(props) {
                     name="ownerFilterVal"
                     placeholder=""
                     className="form-field w-100"
-                    value={ownerFilterVal}
-
                   />
                 </div>
                 <div className="col-lg-6">
@@ -158,8 +284,6 @@ export default function Criteria(props) {
                     name="ownerFilterVal"
                     placeholder=""
                     className="form-field w-100"
-                    value={ownerFilterVal}
-
                   />
                 </div>
               </div>
@@ -167,33 +291,11 @@ export default function Criteria(props) {
           </div>
         </div>
       </div>
-      {/* <li>
-        <a href="#">Owner</a>
-        <input
-          type="text"
-          name="ownerFilterVal"
-          placeholder=""
-          className="form-field"
-          value={ownerFilterVal}
-          onChange={(e) => setOwnerFilterVal(e.target.value)}
-        />
-      </li> */}
-      {/* <li>
-        <a href="#">Product</a>
-        <input
-          type="text"
-          name="productNameFilterVal"
-          placeholder=""
-          className="form-field"
-          value={productNameFilterVal}
-          onChange={(e) => setProductNameFilterVal(e.target.value)}
-        />
-      </li> */}
 
       <div className="apply-section">
         <button onClick={applyFilter}>Apply</button>
         <button onClick={resetFilter}>Reset</button>
       </div>
-    </div >
+    </div>
   );
 }
