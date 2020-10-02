a5133cc6ae61f914b078179c59606a0df4a67e7e
Dazzed <sameepsheth@gmail.com>
2020-10-02 06:16:06 +0000

    bootstrap integrated

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
 import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    import("jquery").then(($) => {
+      window.$ = window.jQuery = $;
+      return import("bootstrap");
+    });
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
