1062076749eefa1771ec0b6412e4ca965639c7aa
Dazzed <sameepsheth@gmail.com>
2020-10-07 13:33:33 +0000

    ui fliter update

--- pages/contracts/components/desktop/table.js
+++ pages/contracts/components/desktop/table.js
@@ -31,98 +31,151 @@ class Table extends Component {
     );
   }
 
-render() {
-  const { contracts } = this.props;
-  const { showDetailedContractModal, currentSelectedContract } = this.state;
+  render() {
+    const { contracts } = this.props;
+    const { showDetailedContractModal, currentSelectedContract } = this.state;
 
-  return (
-    <>
-      <table className="table table-responsive-md">
-        <thead>
-          <tr className="box-none">
-            <th scope="col">
-              <label className="checkbox">
-                <input className="checkbox__input" type="checkbox" />
-                <svg className="checkbox__check" width="20" height="20">
-                  <polyline points="15 6 10 14 5 11"></polyline>
-                </svg>
-              </label>
-              <img
-                src="/imgs/svg-imgs/dropdown-grey.svg"
-                className="dropdwon-grey"
-                alt="dropdown Icon"
-              />
-            </th>
-            <th scope="col">
-              Contract
-                <img
-                src="/imgs/svg-imgs/dropdown-blue.svg"
-                alt="dropdown Icon"
-              />
-            </th>
-            <th scope="col">
-              Owner Name
-                <img
-                src="/imgs/svg-imgs/dropdown-blue.svg"
-                alt="dropdown Icon"
-              />
-            </th>
-            <th scope="col" className="text-center">
-              Age
-                <img
-                src="/imgs/svg-imgs/dropdown-blue.svg"
-                alt="dropdown Icon"
-              />
-            </th>
-            <th scope="col">
-              Product
-                <img
-                src="/imgs/svg-imgs/dropdown-blue.svg"
-                alt="dropdown Icon"
-              />
-            </th>
-            <th scope="col">
-              Value
-                <img
-                src="/imgs/svg-imgs/dropdown-blue.svg"
-                alt="dropdown Icon"
-              />{" "}
-              <button className="add-btn">+</button>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {contracts.map((contract) => (
-            <tr
-              key={contract.id}
-              onClick={() => this.selectContract(contract)}
-              className="btn-hover"
-            >
-              <td scope="row" className="w-6">
+    return (
+      <>
+        <table className="table table-responsive-md">
+          <thead>
+            <tr className="box-none">
+              <th scope="col">
                 <label className="checkbox">
                   <input className="checkbox__input" type="checkbox" />
                   <svg className="checkbox__check" width="20" height="20">
                     <polyline points="15 6 10 14 5 11"></polyline>
                   </svg>
                 </label>
-              </td>
-              <td className="w-20">{contract.contract}</td>
-              <td className="w-22">{contract.ownerName}</td>
-              <td className="w-22 text-center">{contract.ageO}</td>
-              <td className="w-15">{contract.product}</td>
-              <td className="w-15">{numeral(contract.value).format("$0,0.00")}</td>
+                <img
+                  src="/imgs/svg-imgs/dropdown-grey.svg"
+                  className="dropdwon-grey"
+                  alt="dropdown Icon"
+                />
+              </th>
+              <th scope="col">
+                Contract
+              <div class="dropdown show d-inline-block">
+                  <a class="" href="#" role="button" id="dropdownMenuLink2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <img
+                      src="/imgs/svg-imgs/dropdown-blue.svg"
+                      alt="dropdown Icon"
+                    />
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="dropdownMenuLink2">
+                    <ul className="px-1 m-0">
+                      <li class="dropdown-item px-2" href="#">
+                        <label className="checkbox">
+                          <input className="checkbox__input" type="checkbox" />
+                          <svg className="checkbox__check" width="20" height="20">
+                            <polyline points="15 6 10 14 5 11"></polyline>
+                          </svg>
+                          <span className="ml-2">Select 1</span>
+                        </label>
+                      </li>
+                      <li class="dropdown-item px-2" href="#">
+                        <label className="checkbox">
+                          <input className="checkbox__input" type="checkbox" />
+                          <svg className="checkbox__check" width="20" height="20">
+                            <polyline points="15 6 10 14 5 11"></polyline>
+                          </svg>
+                          <span className="ml-2">Select 2</span>
+                        </label>
+                      </li>
+                      <li class="dropdown-item px-2" href="#">
+                        <label className="checkbox">
+                          <input className="checkbox__input" type="checkbox" />
+                          <svg className="checkbox__check" width="20" height="20">
+                            <polyline points="15 6 10 14 5 11"></polyline>
+                          </svg>
+                          <span className="ml-2">Select 3</span>
+                        </label>
+                      </li>
+                      <li class="dropdown-item px-2" href="#">
+                        <label className="checkbox">
+                          <input className="checkbox__input" type="checkbox" />
+                          <svg className="checkbox__check" width="20" height="20">
+                            <polyline points="15 6 10 14 5 11"></polyline>
+                          </svg>
+                          <span className="ml-2">Select 4</span>
+                        </label>
+                      </li>
+                      <li class="dropdown-item px-2" href="#">
+                        <label className="checkbox">
+                          <input className="checkbox__input" type="checkbox" />
+                          <svg className="checkbox__check" width="20" height="20">
+                            <polyline points="15 6 10 14 5 11"></polyline>
+                          </svg>
+                          <span className="ml-2">Select 5</span>
+                        </label>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </th>
+              <th scope="col">
+                Owner Name
+                <img
+                  src="/imgs/svg-imgs/dropdown-blue.svg"
+                  alt="dropdown Icon"
+                />
+              </th>
+              <th scope="col" className="text-center">
+                Age
+                <img
+                  src="/imgs/svg-imgs/dropdown-blue.svg"
+                  alt="dropdown Icon"
+                />
+              </th>
+              <th scope="col">
+                Product
+                <img
+                  src="/imgs/svg-imgs/dropdown-blue.svg"
+                  alt="dropdown Icon"
+                />
+              </th>
+              <th scope="col">
+                Value
+                <img
+                  src="/imgs/svg-imgs/dropdown-blue.svg"
+                  alt="dropdown Icon"
+                />{" "}
+                <button className="add-btn">+</button>
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-      <DetailedContractModal
-        show={showDetailedContractModal}
-        contractDetails={currentSelectedContract}
-        handleClose={this.toggleDetailedModal}
-      />
-    </>
-  );
-}
+          </thead>
+          <tbody>
+            {contracts.map((contract) => (
+              <tr
+                key={contract.id}
+                onClick={() => this.selectContract(contract)}
+                className="btn-hover"
+              >
+                <td scope="row" className="w-6">
+                  <label className="checkbox">
+                    <input className="checkbox__input" type="checkbox" />
+                    <svg className="checkbox__check" width="20" height="20">
+                      <polyline points="15 6 10 14 5 11"></polyline>
+                    </svg>
+                  </label>
+                </td>
+                <td className="w-22">{contract.contract}</td>
+                <td className="w-22">{contract.ownerName}</td>
+                <td className="w-22 text-center">{contract.ageO}</td>
+                <td className="w-15">{contract.product}</td>
+                <td className="w-15">{numeral(contract.value).format("$0,0.00")}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <DetailedContractModal
+          show={showDetailedContractModal}
+          contractDetails={currentSelectedContract}
+          handleClose={this.toggleDetailedModal}
+        />
+      </>
+    );
+  }
 }
 
 export default Table;
