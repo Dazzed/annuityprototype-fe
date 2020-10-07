0c58944762007dc96108d190532e3856089adef3
Dazzed <sameepsheth@gmail.com>
2020-10-07 12:33:29 +0000

    contract modal popup ui update

--- pages/contracts/components/detailedModal.js
+++ pages/contracts/components/detailedModal.js
@@ -19,8 +19,52 @@ export default function DetailedModal(props) {
         style={{ border: "none", paddingBottom: "0" }}
       ></Modal.Header>
       <Modal.Body className="profile_viewcontentmodal">
-        <div>{contractDetails.contract}</div>
+        <div className="row">
+          <div className="col-lg-4 col-md-4 col-sm-6 col-12">
+            <div className="contract-common-title">
+              <h4>Contract Owner</h4>
+              <h2>{contractDetails.ownerName}</h2>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-4 col-sm-6 col-12">
+            <div className="contract-common-title">
+              <h4>Contract Number</h4>
+              <h2>{contractDetails.contract}</h2>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-4 col-sm-6 col-12">
+            <div className="contract-common-title">
+              <h4>Value as of 10/1/2020</h4>
+              <h2>{contractDetails.value}</h2>
+            </div>
+          </div>
+        </div>
+        <div className="row mt-pils">
+          <div className="col-lg-12 col-md-12 col-sm-12 col-12">
+            <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+              <li class="nav-item">
+                <a class="nav-link-custom" id="pills-information-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Information</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link-custom active" id="pills-financial-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Financial Transactions</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link-custom" id="pills-non-financial-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Non-financial Transactions</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link-custom" id="pills-document-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Documents</a>
+              </li>
+            </ul>
+            <div class="tab-content tab-custom-content" id="pills-tabContent">
+              <div class="tab-pane fade show active column-width-pills" id="pills-home" role="tabpanel" aria-labelledby="pills-information-tab">
+              </div>
+              <div class="tab-pane fade column-width-pills" id="pills-profile" role="tabpanel" aria-labelledby="pills-financial-tab"></div>
+              <div class="tab-pane fade column-width-pills" id="pills-contact" role="tabpanel" aria-labelledby="pills-non-financial-tab"></div>
+              <div class="tab-pane fade column-width-pills" id="pills-contact" role="tabpanel" aria-labelledby="pills-non-document-tab"></div>
+            </div>
+          </div>
+        </div>
       </Modal.Body>
-    </Modal>
+    </Modal >
   );
 }
