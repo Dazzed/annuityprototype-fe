1ade42ea33eb14682e12ca4d2713374e1d104a7f
Dazzed <sameepsheth@gmail.com>
2020-10-13 12:05:49 +0000

    notification ui added

---
+++ pages/contracts/components/notifications.js
@@ -0,0 +1,199 @@
+import React from "react";
+
+
+render() {
+    return (
+    <div className="container-fluid">
+        <div className="row">
+            <div className="col-lg-12">
+                <div className="search-box support-searchbox">
+                    <input type="text" placeholder="Search">
+                    <!-- <i id="icon" className="search"></i> -->
+                    <img src="./css/imgs/svg_imgs/support-searchicon.svg" id="icon" className="search searchicon-support">
+                </div>
+            </div>
+        </div>
+        <div className="row mt-pils">
+            <div className="col-lg-12 col-md-12 col-sm-12 col-12">
+                <ul className="nav nav-pills nav-custom-pills nav-pills-support mb-3" id="pills-tab" role="tablist">
+                    <li className="nav-item">
+                        <a className="nav-link-custom active" id="pills-notifications-tab" data-toggle="pill"
+                            href="#pills-notifications" role="tab" aria-controls="pills-notifications"
+                            aria-selected="true">
+                            Notifications
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link-custom" id="pills-tutorial-tab" data-toggle="pill"
+                            href="#pills-tutorial" role="tab" aria-controls="pills-tutorial" aria-selected="false">
+                            Tutorials
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link-custom" id="pills-help" data-toggle="pill" href="#pills-help" role="tab"
+                            aria-controls="pills-help" aria-selected="false">
+                            Help
+                        </a>
+                    </li>
+                </ul>
+                <div className="tab-content tab-custom-supportsection" id="pills-tabContent">
+                    <div className="tab-pane fade show active col-width-suportsection"
+                        id="pills-notifications" role="tabpanel" aria-labelledby="pills-notifications-tab">
+                        <div className="row row-height-supportsection">
+                            <div className="col-lg-2 col-md-2 col-sm-2 col-12 align-self-center">
+                                <div className="userPicSection">
+                                    <img src="./css/imgs/profile-pic.png" alt="user picture" />
+                                </div>
+                            </div>
+                            <div className="col-lg-8 col-md-8 col-sm-7 col-12 align-self-center">
+                                <div className="date-section">
+                                    <h6>October 12 12:55pm</h6>
+                                </div>
+                                <div className="msg-contentsection">
+                                    <p><b>Jane Doe</b> initiatied a withdrawal for $1,500 from contract
+                                        <b>XX123456789</b> via form</p>
+                                </div>
+                            </div>
+                            <div className="col-lg-2 col-md-2 col-sm-3 col-12 align-self-center">
+                                <div className="seemore-section">
+                                    <h6>See More <img src="./css/imgs/svg_imgs/rightarrow-icon.svg" alt="Rightarrow Icon " /> </h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row row-height-supportsection">
+                            <div className="col-lg-2 col-md-2 col-sm-2 col-12 align-self-center">
+                                <div className="userPicSection">
+                                    <img src="./css/imgs/profile-pic.png" alt="user picture" />
+                                </div>
+                            </div>
+                            <div className="col-lg-8 col-md-8 col-sm-7 col-12 align-self-center">
+                                <div className="date-section">
+                                    <h6>October 12 12:55pm</h6>
+                                </div>
+                                <div className="msg-contentsection">
+                                    <p><b>Jane Doe</b> initiatied a withdrawal for $1,500 from contract
+                                        <b>XX123456789</b> via form</p>
+                                </div>
+                            </div>
+                            <div className="col-lg-2 col-md-2 col-sm-3 col-12 align-self-center">
+                                <div className="seemore-section">
+                                    <h6>See More <img src="./css/imgs/svg_imgs/rightarrow-icon.svg" alt="Rightarrow Icon " /> </h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row row-height-supportsection">
+                            <div className="col-lg-2 col-md-2 col-sm-2 col-12 align-self-center">
+                                <div className="userPicSection">
+                                    <img src="./css/imgs/profile-pic.png" alt="user picture" />
+                                </div>
+                            </div>
+                            <div className="col-lg-8 col-md-8 col-sm-7 col-12 align-self-center">
+                                <div className="date-section">
+                                    <h6>October 12 12:55pm</h6>
+                                </div>
+                                <div className="msg-contentsection">
+                                    <p><b>Jane Doe</b> initiatied a withdrawal for $1,500 from contract
+                                        <b>XX123456789</b> via form</p>
+                                </div>
+                            </div>
+                            <div className="col-lg-2 col-md-2 col-sm-3 col-12 align-self-center">
+                                <div className="seemore-section">
+                                    <h6>See More <img src="./css/imgs/svg_imgs/rightarrow-icon.svg" alt="Rightarrow Icon " /> </h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row row-height-supportsection">
+                            <div className="col-lg-2 col-md-2 col-sm-2 col-12 align-self-center">
+                                <div className="userPicSection">
+                                    <img src="./css/imgs/profile-pic.png" alt="user picture" />
+                                </div>
+                            </div>
+                            <div className="col-lg-8 col-md-8 col-sm-7 col-12 align-self-center">
+                                <div className="date-section">
+                                    <h6>October 12 12:55pm</h6>
+                                </div>
+                                <div className="msg-contentsection">
+                                    <p><b>Jane Doe</b> initiatied a withdrawal for $1,500 from contract
+                                        <b>XX123456789</b> via form</p>
+                                </div>
+                            </div>
+                            <div className="col-lg-2 col-md-2 col-sm-3 col-12 align-self-center">
+                                <div className="seemore-section">
+                                    <h6>See More <img src="./css/imgs/svg_imgs/rightarrow-icon.svg" alt="Rightarrow Icon " /> </h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row row-height-supportsection">
+                            <div className="col-lg-2 col-md-2 col-sm-2 col-12 align-self-center">
+                                <div className="userPicSection">
+                                    <img src="./css/imgs/profile-pic.png" alt="user picture" />
+                                </div>
+                            </div>
+                            <div className="col-lg-8 col-md-8 col-sm-7 col-12 align-self-center">
+                                <div className="date-section">
+                                    <h6>October 12 12:55pm</h6>
+                                </div>
+                                <div className="msg-contentsection">
+                                    <p><b>Jane Doe</b> initiatied a withdrawal for $1,500 from contract
+                                        <b>XX123456789</b> via form</p>
+                                </div>
+                            </div>
+                            <div className="col-lg-2 col-md-2 col-sm-3 col-12 align-self-center">
+                                <div className="seemore-section">
+                                    <h6>See More <img src="./css/imgs/svg_imgs/rightarrow-icon.svg" alt="Rightarrow Icon " /> </h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row row-height-supportsection">
+                            <div className="col-lg-2 col-md-2 col-sm-2 col-12 align-self-center">
+                                <div className="userPicSection">
+                                    <img src="./css/imgs/profile-pic.png" alt="user picture" />
+                                </div>
+                            </div>
+                            <div className="col-lg-8 col-md-8 col-sm-7 col-12 align-self-center">
+                                <div className="date-section">
+                                    <h6>October 12 12:55pm</h6>
+                                </div>
+                                <div className="msg-contentsection">
+                                    <p><b>Jane Doe</b> initiatied a withdrawal for $1,500 from contract
+                                        <b>XX123456789</b> via form</p>
+                                </div>
+                            </div>
+                            <div className="col-lg-2 col-md-2 col-sm-3 col-12 align-self-center">
+                                <div className="seemore-section">
+                                    <h6>See More <img src="./css/imgs/svg_imgs/rightarrow-icon.svg" alt="Rightarrow Icon " /> </h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row row-height-supportsection">
+                            <div className="col-lg-2 col-md-2 col-sm-2 col-12 align-self-center">
+                                <div className="userPicSection">
+                                    <img src="./css/imgs/profile-pic.png" alt="user picture" />
+                                </div>
+                            </div>
+                            <div className="col-lg-8 col-md-8 col-sm-7 col-12 align-self-center">
+                                <div className="date-section">
+                                    <h6>October 12 12:55pm</h6>
+                                </div>
+                                <div className="msg-contentsection">
+                                    <p><b>Jane Doe</b> initiatied a withdrawal for $1,500 from contract
+                                        <b>XX123456789</b> via form</p>
+                                </div>
+                            </div>
+                            <div className="col-lg-2 col-md-2 col-sm-3 col-12 align-self-center">
+                                <div className="seemore-section">
+                                    <h6>See More <img src="./css/imgs/svg_imgs/rightarrow-icon.svg" alt="Rightarrow Icon " /> </h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="tab-pane fade column-width-pills col-width-suportsection" id="pills-tutorial"
+                        role="tabpanel" aria-labelledby="pills-tutorial-tab"></div>
+                    <div className="tab-pane fade column-width-pills col-width-suportsection" id="pills-help"
+                        role="tabpanel" aria-labelledby="pills-help"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    );
+}
