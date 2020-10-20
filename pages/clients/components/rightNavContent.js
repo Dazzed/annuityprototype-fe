b30856aecef0d144ae18e505c5099106bce25d53
Dazzed <sameepsheth@gmail.com>
2020-10-20 13:21:47 +0000

    clients page right nav integration

--- pages/clients/components/rightNavContent.js
+++ pages/clients/components/rightNavContent.js
@@ -2,12 +2,6 @@ import format from "date-fns/format";
 import numeral from "numeral";
 
 export default function RightNavContent(props) {
-  const { notification } = props;
-
-  if (!notification) return null;
-
-  const { contract } = notification;
-
   return (
     <div className="row hl-100">
       <div className="col-lg-12 col-md-12 col-sm-12 col-12 mt-pils pt-info-noti">
@@ -65,51 +59,7 @@ export default function RightNavContent(props) {
             id="pills-infos"
             role="tabpanel"
             aria-labelledby="pills-info-tab"
-          >
-            <div className="row">
-              <div className="col-lg-4 col-md-4 col-sm-2 col-12 align-self-center">
-                <div className="userPicSection-info">
-                  <img src="/imgs/profile-pic.png" alt="user picture" />
-                </div>
-              </div>
-              <div className="col-lg-8 col-md-8 col-sm-7 col-12 align-self-center">
-                <div className="client-information-section-info">
-                  <h6>{contract.ownerName}</h6>
-                  <p>Client Information</p>
-                </div>
-                <div className="msg-contentsection">
-                  <p></p>
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-lg-5 col-md-5 col-sm-12 col-12 align-self-center">
-                <div className="client-information-section-info">
-                  <h6>{numeral(contract.value).format("$0,0.00")}</h6>
-                  <p>Withdrawal</p>
-                </div>
-              </div>
-              <div className="col-lg-7 col-md-7 col-sm-12 col-12 align-self-center">
-                <div className="client-information-section-info">
-                  <h6>{contract.contract}</h6>
-                  <p>Contract Number</p>
-                </div>
-              </div>
-            </div>
-            <div className="date-section-info">
-              <p>{format(new Date(), "MMMM d h:mm aaaa")}</p>
-            </div>
-            <br />
-            <br />
-            <br />
-            <br />
-            <div className="recent-activity-info">
-              <p>Recent Activity</p>
-            </div>
-            <div className="chat-support-info">
-              <button>Chat with Support</button>
-            </div>
-          </div>
+          ></div>
           <div
             className="tab-pane fade column-width-pills col-width-suportsection"
             id="pills-forms"
