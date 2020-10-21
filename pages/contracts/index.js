cbe204b920d212d79f37936ccb5b53956242e93e
Dazzed <sameepsheth@gmail.com>
2020-10-21 10:58:05 +0000

    ui search bar fixed

--- pages/contracts/index.js
+++ pages/contracts/index.js
@@ -128,7 +128,7 @@ class ContractsPage extends React.Component {
       <div
         className={`container-fluid h-100 ${
           isNavOpen ? "table-padding-sideNavLeft" : "pl-3"
-        }`}
+        } ${rightNavState !== 0 ? "table-container-rightNav" : ""} `}
       >
         <Head>
           <meta charSet="utf-8" />
